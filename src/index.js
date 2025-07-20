91d5ffd25d5bda732b6472d5ab13f67ced13df90
OldPole <ipetr6828@gmail.com>
2025-07-20 23:04:49 +0300

    added fetch request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log(123);
+fetch('http://localhost:3001/api/marvel/comics')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Marvel API Data:', data);
+  })
+  .catch(error => {
+    console.error('Fetch Error:', error);
+  });
